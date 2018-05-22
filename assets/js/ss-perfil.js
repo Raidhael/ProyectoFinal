62b285ad8b1685ba688226a08b01014df20fcd8d
Salt24 <s.salt.moya@gmail.com>
2018-05-22 07:40:32 +0200

    Falta accion editar - perfil

--- assets/js/ss-perfil.js
+++ assets/js/ss-perfil.js
@@ -6,16 +6,41 @@ $(function () {
 
 function ss_editable(){
     $('.btn').click(function (){
-        if ($(this).parent().hasClass('img-perfil'))
-        var $id = $(this).parent().find('.ss-perfil-titulo').attr('id');
+        var $boton = $(this);
+        if ($($boton).parent().hasClass('img-perfil'))
+        var $id = $($boton).parent().find('.ss-perfil-titulo').attr('id');
         else
-        var $id = $(this).parent().find('.ss-perfil-datos').attr('id');
-        ss_editar($id);
+        var $id = $($boton).parent().find('.ss-perfil-datos').attr('id');
+
+        $($boton).addClass('ss-fade-out');
+        $.get({
+            url: 'includes/ajax/botonesPerfil.ajax.php',
+            success: function (data){
+                $($boton).parent().append(data);
+            },
+            complete: function () {
+                $($boton).css('display','none');
+                $('#acciones').addClass('ss-fade-in');
+
+                $('.done').click(ss_editar($id));
+                $('.cancel').click(function(){
+                    $($boton).css('display','inline-block').removeClass('ss-fade-out').addClass('ss-fade-in');
+                    $('#acciones').remove();
+                });
+
+            }
+        })
+        
     })
 }
 
-
+/*FUNCIONES*/
 function ss_editar(id){
-    var val=$('#'+id).html();
-    alert(val);
+    if (id == 'img_perfil')
+        var val =$('#'+id+'~figure img').attr('src');
+    else
+        var val=$('#'+id).html();
+
+
+        alert(val);
 }
---
+++ includes/ajax/botonesPerfil.ajax.php
@@ -0,0 +1,6 @@
+<?php
+echo '<div id="acciones" class="pull-right">';
+echo '<button class="btn pull-right cancel"> '.file_get_contents('../../assets/images/SVG/cancelar.svg').'</button>';
+echo '<button class="btn pull-right done"> '.file_get_contents('../../assets/images/SVG/done.svg').'</button>';
+echo '</div>';
+?>
